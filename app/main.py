2a4ddeea21d9a764b035cc5fc5640b51c188e005
Sasha Shvaiuk <shvaiuk2310@gmail.com>
2025-02-09 17:48:53 +0200

    add person

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = age
 
 
 def create_person_list(people: list) -> list:
     people_list = []
     for person in people:
+        person = Person(person["name"], person["age"])
         people_list.append(person)
+
     return people_list
